af1131d04ce94a5f288bd693c13abf2777a9ebbc
John Early <john.early3@outlook.com>
2019-05-17 16:34:49 -0600

    Create Projects

---
+++ src/Spendix.Web/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Spendix.Web.ViewModels;
+
+namespace Spendix.Web.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpGet, Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet, Route("Privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet, Route("Error")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
