429f3f9d7106fe80c643c7bf4e64dc3ad7528bdf
John Early <john.early3@outlook.com>
2019-05-18 20:44:36 -0600

    mock accounts nav

---
+++ src/Spendix.Web/Controllers/BankAccountApiController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Spendix.Core.Accessors;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Spendix.Web.Controllers
+{
+    [Authorize]
+    [Route("api/BankAccounts")]
+    public class BankAccountApiController : BaseController
+    {
+        private readonly ILoggedInUserAccountAccessor loggedInUserAccountAccessor;
+
+        public BankAccountApiController(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            loggedInUserAccountAccessor = serviceProvider.GetService<ILoggedInUserAccountAccessor>();
+        }
+
+        [HttpGet, Route("")]
+        public async Task<IActionResult> Get()
+        {
+            var dummyBankAccounts = new List<dynamic>
+            {
+                new { bankAccountId = Guid.NewGuid(), name = "Checking", detailUrl = $"/BankAccount/{Guid.NewGuid()}" },
+                new { bankAccountId = Guid.NewGuid(), name = "Savings", detailUrl = $"/BankAccount/{Guid.NewGuid()}" }
+            };
+
+            return Json(dummyBankAccounts);
+        }
+    }
+}
---
+++ src/Spendix.Web/wwwroot/js/views/shared/layout.js
@@ -0,0 +1,45 @@
+﻿var vm;
+
+$(document).ready(function () {
+    vm = new Vue({
+        el: '#accordionSidebar',
+        data: {
+            loading: true,
+            bankAccounts: []
+        },
+        methods: {
+            getBankAccounts() {
+                //Vue.http.get('/api/BankAccounts').then(function (response) {
+                //    this.data = response.data;
+                //});
+
+                fetch('/api/BankAccounts')
+                    .then(
+                        function (response) {
+                            if (response.status !== 200) {
+                                console.log('Error. Status Code: ' + response.status);
+                                return;
+                            }
+
+                            response.json().then(function (data) {
+                                vm.loading = false;
+                                vm.bankAccounts = data;
+                            });
+                        }
+                    )
+                    .catch(function (err) {
+                        console.log('Fetch Error :-S', err);
+                    });
+            },
+            toggleSidebar() {
+                $("body").toggleClass("sidebar-toggled");
+                $(".sidebar").toggleClass("toggled");
+                if ($(".sidebar").hasClass("toggled")) {
+                    $('.sidebar .collapse').collapse('hide');
+                };
+            }
+        }
+    });
+
+    vm.getBankAccounts();
+});
