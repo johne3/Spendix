e963fb02cc992abc2fd373bdd6281a8db9b22054
John Early <john.early3@outlook.com>
2019-06-07 14:49:21 -0600

    start categories

---
+++ src/Spendix.Web/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Spendix.Web.Controllers
+{
+    [Authorize]
+    [Route("Categories")]
+    public class CategoryController : BaseController
+    {
+        public CategoryController(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        [HttpGet, Route("")]
+        public IActionResult Items()
+        {
+            return View();
+        }
+    }
+}
