3f6838d8d19d7c9c6ed5574aefec7426bb7c7765
John Early <john.early3@outlook.com>
2019-07-04 14:50:45 -0600

    start categories

--- src/Spendix.Web/Controllers/CategoryController.cs
+++ src/Spendix.Web/Controllers/CategoryController.cs
@@ -4,6 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Spendix.Core.Repos;
+using Microsoft.Extensions.DependencyInjection;
+using Spendix.Web.ViewModels.Category;
+using Spendix.Core.Constants;
+using Microsoft.AspNetCore.Http;
+using Spendix.Web.Models;
 
 namespace Spendix.Web.Controllers
 {
@@ -11,14 +17,31 @@ namespace Spendix.Web.Controllers
     [Route("Categories")]
     public class CategoryController : BaseController
     {
+        private readonly BankAccountTransactionCategoryRepo bankAccountTransactionCategoryRepo;
+
         public CategoryController(IServiceProvider serviceProvider) : base(serviceProvider)
         {
+            bankAccountTransactionCategoryRepo = serviceProvider.GetService<BankAccountTransactionCategoryRepo>();
         }
 
         [HttpGet, Route("")]
-        public IActionResult Items()
+        public async Task<IActionResult> Categories()
         {
-            return View();
+            var categories = await bankAccountTransactionCategoryRepo.FindByActiveAsync();
+
+            var vm = new CategoriesViewModel
+            {
+                PaymentCategories = categories.Where(x => x.TransactionType == TransactionTypes.Payment).OrderBy(x => x.Name).ToList(),
+                DepositCategories = categories.Where(x => x.TransactionType == TransactionTypes.Deposit).OrderBy(x => x.Name).ToList()
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost, Route("")]
+        public async Task<IActionResult> Categories(IFormCollection form)
+        {
+            return SetAlertMessageAndRedirect("Categories", "Category", "Categories saved!", AlertMessageType.Success);
         }
     }
 }
---
+++ src/Spendix.Web/wwwroot/js/views/category/categories.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $('#addPaymentCategory').on('click', function () {
+        var html = getRowHtml();
+        $('#paymentCategoriesRow').append(html);
+    });
+
+    $('#addDepositCategory').on('click', function () {
+        var html = getRowHtml();
+        $('#depositCategoriesRow').append(html);
+    });
+});
+
+function getRowHtml() {
+    return '<div class="col-md-11">' +
+        '<div class="form-group">' +
+        '<input type="text" id="" name="" class="form-control" placeholder="Name" value="">' +
+        '</div>' +
+        '</div >' +
+        '<div class="col-md-1">' +
+        '<a href="javascript:void(0)" class="float-right mt-2"><i class="fas fa-fw fa-trash"></i></a>' +
+        '</div>';
+}
