b35438e8fd0a76902d8f4b72076d13d63a72c48a
johne3 <john.early3@outlook.com>
2019-08-15 15:52:18 -0600

    start subcategories

---
+++ src/Spendix.Web/ViewModels/SubCategory/SubCategoriesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Spendix.Core.Entities;
+
+namespace Spendix.Web.ViewModels.SubCategory
+{
+    public class SubCategoriesViewModel
+    {
+        public List<BankAccountTransactionCategory> PaymentCategories { get; set; }
+
+        public List<BankAccountTransactionCategory> DepositCategories { get; set; }
+
+        public List<BankAccountTransactionSubCategory> PaymentSubCategories { get; set; }
+
+        public List<BankAccountTransactionSubCategory> DepositSubCategories { get; set; }
+    }
+}
