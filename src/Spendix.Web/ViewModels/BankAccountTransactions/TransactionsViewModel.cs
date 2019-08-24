ed68f72648eb1a8c75ebf518b12d250b2ef67d64
johne3 <john.early3@outlook.com>
2019-08-24 15:49:49 -0600

    fix balance calculation

--- src/Spendix.Web/ViewModels/BankAccountTransactions/TransactionsViewModel.cs
+++ src/Spendix.Web/ViewModels/BankAccountTransactions/TransactionsViewModel.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Spendix.Core.Entities;
-using Spendix.Core.Models.BankAccountTransactionRepoModels;
 
 namespace Spendix.Web.ViewModels.BankAccountTransactions
 {
@@ -14,10 +13,8 @@ namespace Spendix.Web.ViewModels.BankAccountTransactions
 
         public SelectList TransactionTypeSelectList { get; set; }
 
-        public Spendix.Core.Entities.BankAccount BankAccount { get; set; }
+        public BankAccount BankAccount { get; set; }
 
-        public List<BankAccountTransaction> Transactions { get; set; }
-
-        public List<BankAccountTransactionBalanceModel> TransactionBalances { get; set; }
+        public List<(BankAccountTransaction transaction, decimal balance)> TransactionBalances { get; set; }
     }
 }
