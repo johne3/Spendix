465f2333bb47412b31110cccb1f114009644b36e
John Early <john.early3@outlook.com>
2019-08-29 19:43:10 -0600

    Next Release (#12)
    * start categories
    
    * refactor naming
    
    * start categories
    
    * register repos via reflection
    
    * sub category entity. config, repo
    
    * hide budget nav for now
    
    * bug fix
    
    * refactor
    
    * hook up data access
    
    * start subcategories
    
    * setup new sub category ui
    
    * implement delete
    
    * finish category configuration
    
    * hook up sub categories to transactions
    
    * start dashboard
    
    * working on dashboard

--- src/Spendix.Web/Controllers/HomeController.cs
+++ src/Spendix.Web/Controllers/HomeController.cs
@@ -5,7 +5,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Spendix.Core.Accessors;
 using Spendix.Web.ViewModels;
+using Spendix.Web.ViewModels.Home;
+using Microsoft.Extensions.DependencyInjection;
+using Spendix.Core.Repos;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using FluentDateTime;
+using Spendix.Core.Entities;
 
 namespace Spendix.Web.Controllers
 {
@@ -13,14 +20,86 @@ namespace Spendix.Web.Controllers
     [Route("")]
     public class HomeController : BaseController
     {
+        private readonly BankAccountRepo bankAccountRepo;
+        private readonly BankAccountTransactionRepo bankAccountTransactionRepo;
+
         public HomeController(IServiceProvider serviceProvider) : base(serviceProvider)
         {
+            bankAccountRepo = serviceProvider.GetService<BankAccountRepo>();
+            bankAccountTransactionRepo = serviceProvider.GetService<BankAccountTransactionRepo>();
         }
 
         [HttpGet, Route("")]
-        public IActionResult Dashboard()
+        public async Task<IActionResult> Dashboard(Guid? bankAccountId, string range)
         {
-            return View();
+            var bankAccounts = (await bankAccountRepo.FindByLoggedInUserAccountAsync()).OrderBy(x => x.Name);
+            var ranges = await GetRanges();
+
+            if (string.IsNullOrEmpty(range))
+            {
+                range = "CurrentMonth";
+            }
+
+            DateTime startDate;
+            DateTime endDate;
+
+            if (string.Equals(range, "CurrentMonth", StringComparison.InvariantCultureIgnoreCase))
+            {
+                startDate = DateTime.Today.FirstDayOfMonth();
+                endDate = DateTime.Today.LastDayOfMonth();
+            }
+            else if (string.Equals(range, "CurrentMonth", StringComparison.InvariantCultureIgnoreCase))
+            {
+                startDate = DateTime.Today.FirstDayOfYear();
+                endDate = DateTime.Today.LastDayOfYear();
+            }
+            else
+            {
+                var date = DateTime.Parse(range);
+                startDate = date.FirstDayOfMonth();
+                endDate = date.LastDayOfMonth();
+            }
+
+            BankAccount bankAccount = null;
+
+            if (bankAccountId.HasValue)
+            {
+                bankAccount = bankAccounts.Single(x => x.BankAccountId == bankAccountId.Value);
+            }
+
+            var income = await bankAccountTransactionRepo.FindTotalIncomeByDateRange(startDate, endDate, bankAccount);
+            var expenses = await bankAccountTransactionRepo.FindTotalExpensesByDateRange(startDate, endDate, bankAccount);
+            var categoryAmounts = await bankAccountTransactionRepo.FindCategoryTotalsByDateRange(startDate, endDate, bankAccount);
+
+            var vm = new DashboardViewModel
+            {
+                BankAccountSelectList = new SelectList(bankAccounts, "BankAccountId", "Name", bankAccountId),
+                RangeSelectList = new SelectList(ranges, "Value", "Text", range),
+                TotalIncome = income,
+                TotalExpenses = expenses,
+                ChartData = categoryAmounts.OrderBy(x => x.Amount).ToList()
+            };
+
+            return View(vm);
+        }
+
+        private async Task<List<SelectListItem>> GetRanges()
+        {
+            var monthsAndYears = await bankAccountTransactionRepo.FindTransactionMonthsAndYearsByLoggedInUserAccount();
+
+            var ranges = new List<SelectListItem>
+            {
+                new SelectListItem{  Value = "CurrentMonth", Text = "Current Month" },
+                new SelectListItem{  Value = "CurrentYear", Text = "Current Year" }
+            };
+
+            ranges.AddRange(monthsAndYears.Select(x => new SelectListItem
+            {
+                Value = new DateTime(x.Year, x.Month, 1).ToShortDateString(),
+                Text = $"{x.Month}/{x.Year}"
+            }).ToList());
+
+            return ranges;
         }
 
         [HttpGet, Route("Error")]
---
+++ src/Spendix.Web/wwwroot/js/views/home/dashboard.js
@@ -0,0 +1,107 @@
+﻿// Set new default font family and font color to mimic Bootstrap's default styling
+//Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+//Chart.defaults.global.defaultFontColor = '#858796';
+
+$(document).ready(function () {
+    $('#bankAccountId').on('change', function () {
+        window.location = window.location.origin + '?bankAccountId=' + $('#bankAccountId').val() + '&range=' + $('#range').val();
+    });
+
+    $('#range').on('change', function () {
+        window.location = window.location.origin + '?bankAccountId=' + $('#bankAccountId').val() + '&range=' + $('#range').val();
+    });
+
+
+    // Pie Chart Example
+    var ctx = document.getElementById("expensesByCategoryChart");
+    var myPieChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: chartLabels,
+            datasets: [{
+                data: chartData,
+                backgroundColor: chartColors,
+                //backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+                //hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+                hoverBorderColor: "rgba(234, 236, 244, 1)"
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            tooltips: {
+                backgroundColor: "rgb(255,255,255)",
+                bodyFontColor: "#858796",
+                borderColor: '#dddfeb',
+                borderWidth: 1,
+                xPadding: 15,
+                yPadding: 15,
+                displayColors: false,
+                caretPadding: 10,
+            },
+            legend: {
+                display: true,
+                position: 'right',
+                labels: {
+                    //generateLabels: function (chart) {
+                    //    var data = chart.data;
+                    //    if (data.labels.length && data.datasets.length) {
+                    //        return data.labels.map(function (label, i) {
+                    //            var meta = chart.getDatasetMeta(0);
+                    //            //var style = meta.controller.getStyle(i);
+
+                    //            var ds = data.datasets[0];
+                    //            var arc = meta.data[i];
+                    //            var style = arc && arc.custom || {};
+
+                    //            return {
+                    //                text: label,
+                    //                fillStyle: style.backgroundColor,
+                    //                strokeStyle: style.borderColor,
+                    //                lineWidth: style.borderWidth,
+                    //                hidden: isNaN(data.datasets[0].data[i]) || meta.data[i].hidden,
+
+                    //                // Extra data used for toggling the correct item
+                    //                index: i
+                    //            };
+                    //        });
+                    //    }
+                    //    return [];
+                    //}
+                    generateLabels: function (chart) {
+                        var data = chart.data;
+                        if (data.labels.length && data.datasets.length) {
+                            return data.labels.map(function (label, i) {
+                                var meta = chart.getDatasetMeta(0);
+                                var ds = data.datasets[0];
+                                var arc = meta.data[i];
+                                var custom = arc && arc.custom || {};
+                                var getValueAtIndexOrDefault = Chart.helpers.getValueAtIndexOrDefault;
+                                var arcOpts = chart.options.elements.arc;
+                                var fill = custom.backgroundColor ? custom.backgroundColor : getValueAtIndexOrDefault(ds.backgroundColor, i, arcOpts.backgroundColor);
+                                var stroke = custom.borderColor ? custom.borderColor : getValueAtIndexOrDefault(ds.borderColor, i, arcOpts.borderColor);
+                                var bw = custom.borderWidth ? custom.borderWidth : getValueAtIndexOrDefault(ds.borderWidth, i, arcOpts.borderWidth);
+
+                                // We get the value of the current label
+                                var value = chart.config.data.datasets[arc._datasetIndex].data[arc._index];
+
+                                return {
+                                    // Instead of `text: label,`
+                                    // We add the value to the string
+                                    text: label + " : " + value,
+                                    fillStyle: fill,
+                                    strokeStyle: stroke,
+                                    lineWidth: bw,
+                                    hidden: isNaN(ds.data[i]) || meta.data[i].hidden,
+                                    index: i
+                                };
+                            });
+                        } else {
+                            return [];
+                        }
+                    }
+                }
+            },
+            cutoutPercentage: 80    
+        },
+    });
+});
