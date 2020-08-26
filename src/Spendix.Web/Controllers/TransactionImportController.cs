ebfff9db75c2f8a56e068dd3352c01ebb6394478
John Early <john.early3@outlook.com>
2020-08-25 20:39:54 -0600

    create view and hook up layout link

---
+++ src/Spendix.Web/Controllers/TransactionImportController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Spendix.Web.Controllers
+{
+    [Route("Transactions")]
+    [Authorize]
+    public class TransactionImportController : BaseController
+    {
+        public TransactionImportController(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+
+        [HttpGet, Route("Import")]
+        public IActionResult Import()
+        {
+            return View();
+        }
+    }
+}
